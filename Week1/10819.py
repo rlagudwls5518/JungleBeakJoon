ef7143016b7429cb67fd12354983f040f782d2a8
rlagudwls5518 <rlagudwls5518@naver.com>
2025-07-15 01:14:46 +0900

    7/15+

--- Week1/10819.py
+++ Week1/10819.py
@@ -4,18 +4,36 @@
 # 이렇게 해서 더하념된다ㅓ?.
 # 정수의 순서를 적절히 바꿔서
 # |A[0] - A[1]| + |A[1] - A[2]| + ... + |A[N-2] - A[N-1]|
-# 
-#  
+# 어떻게 구상을 할까 
+
+
+from itertools import permutations
+# 순열 만드는 라이브러리
+#-> 모든 경우의수 
+
+
+def calc_diff(arr):
+    total = 0
+    for i in range(len(arr)-1):
+        total += abs(arr[i] - arr[i+1])
+    return total
+#절대값의 합 
+
+
 
 data_count = int(input())
 A=list(map(int, input().split()))
 
+max_value = 0
+for p in permutations(A):
+    max_value = max(max_value, calc_diff(p))
 
+print(max_value)
 
-A.sort()
-sum=0
-for i in range(len(A)):
-    for j in range(len(A) - 1, -1, -1):
-        sum=sum+abs(A[i]-A[j])
-print(A)
+# A.sort()
+# sum=0
+# for i in range(len(A)):
+#     for j in range(len(A) - 1, -1, -1):
+#         sum=sum+abs(A[i]-A[j])
+# print(A)
 
